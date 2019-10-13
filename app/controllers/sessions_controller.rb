479ebbe03336616aa716fa9bb2bf0860b820e391
nkk0508 <keita19920508@gmail.com>
2019-10-13 21:05:18 +0900

    implemented login & logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
 
     if user&.authenticate(session_params[:password])
-      session[:user_id] = user.user_id
-      redirect_to root_url, notice: 'ログインしました。'
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'completed login'
     else
       render :new
     end
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     reset_session
-    redirect_to root_url, notice: 'ログアウトしました。'
+    redirect_to root_url, notice: 'completed logout'
   end
 
   private
