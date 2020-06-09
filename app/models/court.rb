efc7a1ff74c31a28b1bb80354d4ff001ce3e2b3d
nkk0508 <keita19920508@gmail.com>
2020-06-09 20:22:16 +0900

    fix validation

--- app/models/court.rb
+++ app/models/court.rb
@@ -2,7 +2,8 @@ class Court < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
 
-  validates :name, :categories, :non_residents, :postal_code, :address, :location_information, :access_count, :favorite_count, presence: true
+  validates :name, :categories, :postal_code, :address, :location_information, :access_count, :favorite_count, presence: true
+  validates :non_residents, inclusion: [true, false]
 
   def ward
     address = self.address
